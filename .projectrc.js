f6f52bd3a7a85eeb34b19281768a442f1c6a642f
MoYuanJun <moyuanjun925@gmail.coml>
2023-10-02 14:51:19 +0800

    init

---
+++ .projectrc.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default {
+  logo: { 
+    img: void 0,
+    title: '测试标题',
+  },
+  footer: () => ('footer'), 
+  header: () => ('header'), 
+  useHashRouter: true, 
+  menu: [
+    {
+      key: 'home',
+      title: '首页',
+      icon: 'AndroidOutlined',
+      url: '/',
+      routes: [
+        {
+          path: '/',
+          element: <div>首页</div>,
+        },
+      ],
+    },
+  ],
+};
